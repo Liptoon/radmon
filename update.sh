1ad8d5e1e47bb4bf7de661d008fe409a2bcda5fa
Publius <liptoon@gmail.com>
2026-03-20 13:52:39 +0100

    Fix: Fixed update.sh not working as intended.

--- update.sh
+++ update.sh
@@ -2,12 +2,16 @@
 # Radmon Updater
 
 echo "[*] Pulling latest changes from GitHub..."
+git config core.fileMode false
 git pull origin main
+
 
 echo "[*] Updating dependencies in venv..."
 ./venv/bin/pip install --upgrade git+https://github.com/Liptoon/radiacode.git
 
-./venv/bin/pip install -r requirements.txt
+if [ -f "requirements.txt" ]; then
+    ./venv/bin/pip install -r requirements.txt
+fi
 
 chmod +x radmon.py
 
